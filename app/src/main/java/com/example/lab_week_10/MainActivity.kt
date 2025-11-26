90373bcaa4881afd070d63c8ef8ec11b1f93e8b8
Eufrat Algaws <eufrat.ag@gmail.com>
2025-11-26 14:30:31 +0700

    Commit No. 4: Complete Bonus Assignment (Add Update Timestamp)

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.lab_week_10
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.example.lab_week_10.database.Total
 import com.example.lab_week_10.database.TotalDatabase
+import com.example.lab_week_10.database.TotalObject
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +27,16 @@ class MainActivity : AppCompatActivity() {
 
         initializeValueFromDatabase()
         prepareViewModel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val totalData = db.totalDao().getTotal(ID)
+        if (!totalData.isEmpty()) {
+            val lastDate = totalData.first().total.date
+            Toast.makeText(this, lastDate, Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun updateText(total: Int) {
@@ -46,21 +59,41 @@ class MainActivity : AppCompatActivity() {
             applicationContext,
             TotalDatabase::class.java,
             "total-database"
-        ).allowMainThreadQueries().build()
+        )
+            .fallbackToDestructiveMigration()
+            .allowMainThreadQueries()
+            .build()
     }
 
     private fun initializeValueFromDatabase() {
         val total = db.totalDao().getTotal(ID)
         if (total.isEmpty()) {
-            db.totalDao().insert(Total(id = 1, total = 0))
+            db.totalDao().insert(
+                Total(
+                    id = ID,
+                    total = TotalObject(
+                        value = 0,
+                        date = Date().toString()
+                    )
+                )
+            )
         } else {
-            viewModel.setTotal(total.first().total)
+            viewModel.setTotal(total.first().total.value)
         }
     }
 
     override fun onPause() {
         super.onPause()
-        db.totalDao().update(Total(ID, viewModel.total.value ?: 0))
+
+        db.totalDao().update(
+            Total(
+                id = ID,
+                total = TotalObject(
+                    value = viewModel.total.value ?: 0,
+                    date = Date().toString()
+                )
+            )
+        )
     }
 
     companion object {
